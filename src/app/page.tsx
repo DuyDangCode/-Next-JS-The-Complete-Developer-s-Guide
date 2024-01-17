97e7bebc4f0294cfa28d5b4d15a1988a476b20b1
DuyDangCode <21520774@gm.uit.edu.vn>
2024-01-17 23:23:35 +0700

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,23 @@ export default async function Home() {
   const renderedSnippet = allSnippets.map((snippet) => {
     return (
       <div key={snippet.id}>
-        <Link href={`/snippet/${snippet.id}`}>{snippet.title}</Link>
+        <Link
+          href={`/snippet/${snippet.id}`}
+          className=' border-2 flex justify-center items-center p-[10px] min-w-[200px]'
+        >
+          {snippet.title}
+        </Link>
       </div>
     );
   });
   return (
-    <div className='flex flex-col gap-5'>
-      <Link href={'/snippet/new'}>And new</Link>
+    <div className='flex flex-col gap-5 justify-center items-center w-full h-full'>
+      <Link
+        href={'/snippet/new'}
+        className=' rounded-md border-2 bg-blue-200 flex justify-center items-center w-[100px]'
+      >
+        And new
+      </Link>
       {renderedSnippet}
     </div>
   );
